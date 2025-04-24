9e45419114057df4f9816e89c458794f73009310
Azamat Zulpykhar <zlpkhr@icloud.com>
2025-04-24 12:00:56 +0200

    Remove unused consts

--- resources/src/routes/uploads.tsx
+++ resources/src/routes/uploads.tsx
@@ -1,13 +1,7 @@
-import { a } from "#components/aba";
-import { asd } from "#components/bla";
 import { useParams } from "react-router";
 
 export function UploadsRoute() {
   const { eventID } = useParams();
 
-  return (
-    <div>
-      Uploads: {eventID} {a} {asd}
-    </div>
-  );
+  return <div>Uploads: {eventID}</div>;
 }
