f066707114550cfae30596e677831a8fce82a163
Azamat Zulpykhar <zlpkhr@icloud.com>
2025-04-24 11:15:06 +0200

    Setup react router

--- resources/src/main.tsx
+++ resources/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
 import "./main.css";
 
 const rootEl = document.getElementById("root");
@@ -8,4 +9,15 @@ if (!rootEl) {
   throw new Error("Root element not found");
 }
 
-createRoot(rootEl).render(<StrictMode />);
+createRoot(rootEl).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={<h1 className="text-3xl font-serif">Hello World</h1>}
+        />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
