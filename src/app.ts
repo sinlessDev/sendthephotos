46e5579eb664b67dffa19cae09ed4f97b420d1f3
Azamat Zulpykhar <zlpkhr@icloud.com>
2025-04-24 11:31:28 +0200

    Setup resource serving

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,23 @@ export async function createApp(config: Config, deps: { db: DB }) {
     app.use(morgan("tiny"));
   }
 
-  app.use("/uploads", createUploadsRouter(config));
+  if (config.serveResources) {
+    const path = await import("node:path");
+    const { default: compression } = await import("compression");
+
+    app.use(
+      express.static(path.join(import.meta.dirname, "..", "resources", "dist")),
+    );
+    app.use(compression());
+
+    app.get("{*splat}", (req, res) => {
+      res.sendFile(
+        path.join(import.meta.dirname, "..", "resources", "dist", "index.html"),
+      );
+    });
+  }
+
+  app.use("/api/uploads", createUploadsRouter(config));
 
   return app;
 }
--- src/config.ts
+++ src/config.ts
@@ -4,6 +4,7 @@ declare global {
       ENABLE_HTTP_REQUEST_LOGGING: string;
       DATABASE_URL: string;
       FILE_STORE_DIR_PATH: string;
+      SERVE_RESOURCES: string;
     }
   }
 }
@@ -12,6 +13,7 @@ const config = Object.freeze({
   enableHttpRequestLogging: process.env.ENABLE_HTTP_REQUEST_LOGGING === "true",
   databaseURL: process.env.DATABASE_URL,
   fileStoreDirPath: process.env.FILE_STORE_DIR_PATH,
+  serveResources: process.env.SERVE_RESOURCES === "true",
 });
 
 export type Config = typeof config;
