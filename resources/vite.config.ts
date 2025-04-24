9762a8a73914e5fbffc52ba687cbdfd44287577b
Azamat Zulpykhar <zlpkhr@icloud.com>
2025-04-24 12:21:16 +0200

    Add uploads route

--- resources/vite.config.ts
+++ resources/vite.config.ts
@@ -8,6 +8,8 @@ export default defineConfig({
   resolve: {
     alias: {
       "#components": path.resolve(__dirname, "src", "components"),
+      "#lib": path.resolve(__dirname, "src", "lib"),
+      "#hooks": path.resolve(__dirname, "src", "hooks"),
     },
   },
   server: {
