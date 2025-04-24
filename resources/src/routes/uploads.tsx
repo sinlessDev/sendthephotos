6878542cb5efc31b4686e8e51850d1ac83d8a764
Azamat Zulpykhar <zlpkhr@icloud.com>
2025-04-24 12:00:38 +0200

    Add subpath imports

--- resources/src/routes/uploads.tsx
+++ resources/src/routes/uploads.tsx
@@ -1,7 +1,13 @@
+import { a } from "#components/aba";
+import { asd } from "#components/bla";
 import { useParams } from "react-router";
 
 export function UploadsRoute() {
   const { eventID } = useParams();
 
-  return <div>Uploads: {eventID}</div>;
+  return (
+    <div>
+      Uploads: {eventID} {a} {asd}
+    </div>
+  );
 }
--- resources/vite.config.ts
+++ resources/vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import path from "node:path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "#components": path.resolve(__dirname, "src", "components"),
+    },
+  },
   server: {
     proxy: {
       "/api": {
