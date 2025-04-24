3351028f3a8b6133396a6f9b59107bbe62a5b33f
Azamat Zulpykhar <zlpkhr@icloud.com>
2025-04-24 05:35:00 +0200

    Split the server code from app code

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
-import express from "express";
+import app from "./app.ts";
 
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Hello, world!");
+const server = app.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+["SIGINT", "SIGTERM"].forEach((signal) => {
+  process.on(signal, () => {
+    server.close((err) => {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      } else {
+        process.exit(0);
+      }
+    });
+  });
 });
