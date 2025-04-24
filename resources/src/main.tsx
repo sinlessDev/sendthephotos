11e6068e56941838fcaee8b01bc1495a0ba14449
Azamat Zulpykhar <zlpkhr@icloud.com>
2025-04-24 11:13:19 +0200

    Setup tailwind

--- resources/src/main.tsx
+++ resources/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "./main.css";
 
 const rootEl = document.getElementById("root");
 
--- resources/vite.config.ts
+++ resources/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
 });
