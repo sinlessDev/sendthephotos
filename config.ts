5326f8c00cde4f0c54a859af521ee6f764f4bdd6
Azamat Zulpykhar <zlpkhr@icloud.com>
2025-04-27 03:59:58 +0200

    Cleanup

--- config.ts
+++ config.ts
@@ -9,19 +9,20 @@ export type Config = {
 };
 
 const envSchema = v.object({
-  NODE_ENV: v.literal("production"),
   HOST: v.string(),
   PORT: v.pipe(v.string(), v.transform(parseInt)),
   ENABLE_HTTP_REQUEST_LOGGING: v.pipe(
     v.union([v.literal("true"), v.literal("false")]),
-    v.transform((input) => input === "true"),
+    v.transform((input) => input === "true")
   ),
   FORCE_SHUTDOWN_TIMEOUT_SEC: v.pipe(v.string(), v.transform(parseInt)),
   DATABASE_URL: v.string(),
 });
 
+process.env.NODE_ENV = "production";
+
 export function createConfigFromEnv(
-  env: Record<string, string | undefined> = process.env,
+  env: Record<string, string | undefined> = process.env
 ): Config {
   const safeEnv = v.parse(envSchema, env);
 
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,22 @@
 import { defineConfig } from "drizzle-kit";
-import { createConfigFromEnv } from "./config.ts";
 
-const config = createConfigFromEnv();
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      DATABASE_URL: string;
+    }
+  }
+}
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not set");
+}
 
 export default defineConfig({
   out: "./drizzle",
   schema: "./db.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: config.databaseURL,
+    url: process.env.DATABASE_URL,
   },
 });
